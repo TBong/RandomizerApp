bd3aed677aff08c4500319a13a6295807120f8c2
TBong <bataevarsenij5@gmail.com>
2022-10-20 22:14:08 +0300

    release v1

--- RandomizerApp/Form1.cs
+++ RandomizerApp/Form1.cs
@@ -16,5 +16,35 @@ namespace RandomizerApp
         {
             InitializeComponent();
         }
+        private void ButtGen_Click(object sender, EventArgs e)
+        {
+            int from = Int32.Parse(textBox1.Text);
+            int to = Int32.Parse(textBox2.Text);
+
+            Random rnd = new Random();
+
+            int res = rnd.Next(from, to+1);
+
+            result.Text  = res.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void result_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
